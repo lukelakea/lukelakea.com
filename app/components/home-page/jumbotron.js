93fb753d0fbda584b26a4122ba1ad94371a46a8c
chrisdevor <chrisdevor@macroplant.com>
2020-07-21 21:28:56 -0400

    character anaimations

--- app/components/home-page/jumbotron.js
+++ app/components/home-page/jumbotron.js
@@ -14,19 +14,23 @@ export default class extends Component {
   didInsertElement() {
     this._super(...arguments);
     var tl = anime.timeline({loop: true})
-    this.animatedSubtitles.forEach(function(t, idx){
+    this.animatedSubtitles.forEach((t, idx) => {
+      var textWrapper = this.$(`.letters.letters-${idx}`).get(0);
+      textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, `<span class='letter'>$&</span>`);
       tl.add({
-        targets: `.letters.letters-${idx}`,
+        targets: `.letters.letters-${idx} .letter`,
         opacity: [0,1],
-        scale: [0.2, 1],
-        duration: 800,
+        // scale: [0.2, 1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 150 * (i+1)
       }).add({
         targets: `.letters.letters-${idx}`,
         opacity: 0,
-        scale: 3,
-        duration: 500,
-        easing: "easeInExpo",
-        delay: 500
+        // scale: 3,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
       });
     });
   }
