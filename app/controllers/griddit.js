d30e64f2bd0db726a6bbbc1eb56e3e83b70f2c88
Luke Lakea <lukelakea@macroplant.com>
2020-07-26 22:56:45 -0400

    subreddit drop down box working on submit

---
+++ app/controllers/griddit.js
@@ -0,0 +1,40 @@
+import Controller from '@ember/controller';
+import Ember from 'ember';
+import { action } from '@ember/object';
+
+var sorting = 'top';
+var time = 'all';
+
+
+export default Controller.extend({
+  subredditName: 'imaginarymonsters',
+  subredditData: [],
+  actions:{
+    update: function(value) {
+      this.set("subredditName", value);
+    },
+    submit: function(value){
+      this.subredditSubmitAction();
+    }
+  },
+  init(){
+    this._super(...arguments);
+    this.subredditSubmitAction();
+  },
+  ajax: Ember.inject.service(),
+  subredditSubmitAction(){
+    // console.log(this.get('ajax').request('https://www.reddit.com/r/' + this.subredditName + '.json?sort=' + sorting + '&t=' + time + ''));
+    this.get('ajax').request('https://www.reddit.com/r/' + this.subredditName + '/' + sorting + '.json?t=' + time).then((res)=>{
+      this.set("subredditData", res);
+    });
+  },
+  filteredChildren: Ember.computed('subredditData',function(){
+    return Ember.A(this.get('subredditData.data.children'))
+    .filter((c)=>{
+      var imgLink = c.data.url;
+      if(imgLink.search(".jpg")>0 || imgLink.search(".jpg")>0 ){
+        return c;
+      }
+    });
+  })
+})
--- app/routes/griddit.js
+++ app/routes/griddit.js
@@ -1,3 +1,4 @@
-import Route from "@ember/routing/route";
+import Route from '@ember/routing/route';
 
-export default class GridditRoute extends Route {}
+export default Ember.Route.extend({
+});
