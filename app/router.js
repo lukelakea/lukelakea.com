784f18002183522da88c02922478b3b6eaf075ed
chrisdevor <chrisdevor@macroplant.com>
2020-08-03 19:54:18 -0400

    remove implied route

--- app/router.js
+++ app/router.js
@@ -9,7 +9,6 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('griddit', function(){
     this.route('subreddit', {path: '/:subreddit_id'});
-    this.route('index', {path: "/"});
   });
   this.route("home-page", { path: "/" });
 });
