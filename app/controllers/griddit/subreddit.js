59c55805e6d154199506fa835fe122c6d9abc035
chrisdevor <chrisdevor@macroplant.com>
2020-08-03 20:32:41 -0400

    use thumbnails + cleanup filter

--- app/controllers/griddit/subreddit.js
+++ app/controllers/griddit/subreddit.js
@@ -29,12 +29,8 @@ export default Controller.extend({
     },
   },
   filteredChildren: Ember.computed('model',function(){
-    return Ember.A(this.get('model.data.children'))
-    .filter((c)=>{
-      var imgLink = c.data.url;
-      if(imgLink.search(".jpg")>0 || imgLink.search(".png")>0 || imgLink.search(".gif")>0 ){
-        return c;
-      }
+    return Ember.A(this.get('model.data.children')).filter((c)=>{
+      return c.data.thumbnail && c.data.thumbnail.match(/\.(jpg|png|gif)($|\?)/)
     });
   })
 })
